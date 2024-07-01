efd0bcf2d50fc9e8f7cb19fd6c10be877c84d58d
Jason8908 <jasonsu894@gmail.com>
2024-07-01 15:44:59 -0400

    bug: Fixed page automatically re-logging user in after logout.

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -3,7 +3,7 @@ import {
   SocialAuthService,
   GoogleSigninButtonModule,
 } from '@abacritt/angularx-social-login';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthWrapperComponent } from "../../components/auth-wrapper/auth-wrapper.component";
 import { GoogleLoginProvider } from "@abacritt/angularx-social-login";
 import { ApiService } from '../../services/api.service';
@@ -19,10 +19,16 @@ import { CookieService } from 'ngx-cookie-service';
     imports: [GoogleSigninButtonModule, AuthWrapperComponent]
 })
 export class HomeComponent {
-  constructor(private authService: SocialAuthService, private router: Router, private apiService: ApiService, private cookieService: CookieService) {}
-
+  signOut: boolean = false;
+  constructor(private authService: SocialAuthService, private router: Router, private route: ActivatedRoute, private apiService: ApiService, private cookieService: CookieService) {
+    this.route.queryParamMap.subscribe(params => {
+      this.signOut = params.get('signOut') === 'true';
+    });
+  }
   ngOnInit(): void {
     this.authService.authState.subscribe((user) => {
+      if (this.signOut)
+        return;
       // Get an access token from the GoogleLoginProvider.
       this.authService.getAccessToken(GoogleLoginProvider.PROVIDER_ID).then(token => {
         // Send the access token to the API.
