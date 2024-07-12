5f0f48a4c8b3fc8cf5db5b76608d1539879070a7
Miles Bernstein <milesbernstein48@gmail.com>
2024-07-12 14:54:00 -0400

    Saved jobs UI Done

--- client/src/app/pages/saved-jobs/saved-jobs.component.ts
+++ client/src/app/pages/saved-jobs/saved-jobs.component.ts
@@ -4,8 +4,7 @@ import { CommonModule } from '@angular/common';
 import { JobPreviewComponent } from '../../components/job-preview/job-preview.component';
 import { JobDescriptionComponent } from '../../components/job-description/job-description.component';
 import { ApiService } from '../../services/api.service';
-
-const PAGE_LENGTH = 10;
+import { PageEvent, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-saved-jobs',
@@ -15,6 +14,7 @@ const PAGE_LENGTH = 10;
     CommonModule,
     JobPreviewComponent,
     JobDescriptionComponent,
+    MatPaginatorModule,
   ],
   templateUrl: './saved-jobs.component.html',
   styleUrl: './saved-jobs.component.css',
@@ -22,7 +22,10 @@ const PAGE_LENGTH = 10;
 export class SavedJobsComponent {
   public savedJobs: any = [];
   public currentJob: any = undefined;
-  private page: number = 0;
+  public pageSize: number = 10;
+  public pageIndex: number = 0;
+  public length: number = 0;
+  public pageEvent: any;
 
   constructor(private apiService: ApiService) {}
 
@@ -32,7 +35,7 @@ export class SavedJobsComponent {
 
   getJobs() {
     this.apiService
-      .getSavedJobs(PAGE_LENGTH * this.page, PAGE_LENGTH)
+      .getSavedJobs(this.pageSize * this.pageIndex, this.pageSize)
       .subscribe((res) => {
         console.log(res);
         this.savedJobs = res.data.jobs.map((job: { Job: any }) => job.Job);
@@ -41,20 +44,19 @@ export class SavedJobsComponent {
         });
 
         this.currentJob = this.savedJobs[0];
+        this.length = res.data.totalCount;
       });
   }
 
-  nextPage() {
-    this.page++;
-    this.getJobs();
-  }
-
-  prevPage() {
-    this.page--;
-    this.getJobs();
-  }
-
   ngOnInit() {
+    this.getJobs();
+  }
+
+  handlePageEvent(e: PageEvent) {
+    this.pageEvent = e;
+    this.length = e.length;
+    this.pageSize = e.pageSize;
+    this.pageIndex = e.pageIndex;
     this.getJobs();
   }
 }
